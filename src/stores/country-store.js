191d0bfa9a8807add9f1e258816eef7d49c458af
Nick Harbaugh <nickharb@nickssqlywiggly.lan>
2021-09-25 10:39:53 -0500

    Basic force layout logic - bug

--- src/stores/country-store.js
+++ src/stores/country-store.js
@@ -3,7 +3,7 @@ import {writable} from 'svelte/store'; // imports stores from main svelte code
 // call writable function to create a store, pass a default value as an argument
 export const hoveredCountry = writable();
 export const hoveredArea = writable();
-export const selectedCountry = writable('china-2010');
+export const selectedCountry = writable('canada');
 export const selectedArea = writable();
 export const hoveredInfo = writable();
 
