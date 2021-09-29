23d25f9c44a88d2932bf7df12b3275ac76f59f12
Nick Harbaugh <nickharb@nickssqlywiggly.lan>
2021-09-28 20:13:10 -0500

    latest working code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import App from './App.svelte';
 
 var app = new App({
-    // target: document.body
-    target: document.getElementById('pathfinder-dashboard-container')
+    target: document.body
+    // target: document.getElementById('pathfinder-dashboard-container')
 });
 
 export default app;
